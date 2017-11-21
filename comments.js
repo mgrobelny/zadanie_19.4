824ed095787b2f4e11bde67e3b7f7faaf26b7533
mgrobelny <michalmaciejg@gmail.com>
2017-11-21 17:28:59 +0100

    2nd commit

--- comments.js
+++ comments.js
@@ -5,8 +5,9 @@ import {DELETE_COMMENT} from './actions';
 import {ADD_LIKE_TO_COMMENT} from './actions';
 import {ADD_UNLIKE_TO_COMMENT} from './actions'
 
-function reducer(state = initialState, action) {
+function reducer(state = [], action) {
 	switch(action.type) {
+		
 		case ADD_COMMENT:
 			return Object.assign({}, state, {
 				comments: [
@@ -17,32 +18,24 @@ function reducer(state = initialState, action) {
 				}
 				, ...state]
 			});
+
 		case REMOVE_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.filter(comment => comment.id !== action.id)
-            });
+            return [ state.filter(comment => comment.id !== action.id)];
+
         case EDIT_COMMENT:
-        	return state.comments.filter(comment => {
-        		if (comment.id === action.id) {
-        			comment.text = acton.text;
-        		}
-        	});
+        	return [ state.map(comment => {
+        		return comment.id === action.id ? Object.assign({}, comment, { text: action.text }) : comment })
+        ];
+        
         case ADD_LIKE_TO_COMMENT:
-        	return state.map(comment => {
-        		if (comment.id === action.id) {
-        			return Object.assign({}, comment, {
-        				votes: comment.votes + 1
-        			});
-        		}
-        	});
+        	return [ state.map(comment => { 
+        		return comment.id === action.id ? Object.assign({}, comment, { votes: comment.votes + 1 }) : comment })
+        ];
+
         case ADD_UNLIKE_TO_COMMENT:
-        	return state.map(comment => {
-        		if (comment.id === action.id) {
-        			return Object.assign({}, comment, {
-        				votes: comment.votes - 1
-        			});
-        		};
-        	});
+			return [ state.map(comment => { 
+        		return comment.id === action.id ? Object.assign({}, comment, { votes: comment.votes - 1 }) : comment })
+        ];
         	
 		default:
 			return state;
