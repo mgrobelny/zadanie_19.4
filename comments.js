de8a554a5a98f2f05fa1740fe38a0ce80a42b698
mgrobelny <michalmaciejg@gmail.com>
2017-11-22 19:27:02 +0100

    3rd commit

--- comments.js
+++ comments.js
@@ -1,41 +1,31 @@
 
-import {ADD_COMMENT} from './actions';
-import {EDIT_COMMENT} from './actions';
-import {DELETE_COMMENT} from './actions';
-import {ADD_LIKE_TO_COMMENT} from './actions';
-import {ADD_UNLIKE_TO_COMMENT} from './actions'
+import {ADD_COMMENT, EDIT_COMMENT, DELETE_COMMENT, ADD_LIKE_TO_COMMENT, ADD_UNLIKE_TO_COMMENT} from './actions';
 
 function reducer(state = [], action) {
 	switch(action.type) {
 		
 		case ADD_COMMENT:
-			return Object.assign({}, state, {
-				comments: [
-				{
-					id: action.id,
-					text: action.text,
-					votes: 0
-				}
-				, ...state]
-			});
+			return [{
+				id: action.id,
+				text: action.text,
+				votes: 0
+			}
+			, ...state];
 
-		case REMOVE_COMMENT:
-            return [ state.filter(comment => comment.id !== action.id)];
+		case DELETE_COMMENT:
+            return state.filter(comment => comment.id !== action.id);
 
         case EDIT_COMMENT:
-        	return [ state.map(comment => {
-        		return comment.id === action.id ? Object.assign({}, comment, { text: action.text }) : comment })
-        ];
+        	return state.map(comment => {
+        		return comment.id === action.id ? Object.assign({}, comment, { text: action.text }) : comment });
         
         case ADD_LIKE_TO_COMMENT:
-        	return [ state.map(comment => { 
-        		return comment.id === action.id ? Object.assign({}, comment, { votes: comment.votes + 1 }) : comment })
-        ];
+        	return state.map(comment => { 
+        		return comment.id === action.id ? Object.assign({}, comment, { votes: comment.votes + 1 }) : comment });
 
         case ADD_UNLIKE_TO_COMMENT:
-			return [ state.map(comment => { 
-        		return comment.id === action.id ? Object.assign({}, comment, { votes: comment.votes - 1 }) : comment })
-        ];
+			return state.map(comment => { 
+        		return comment.id === action.id ? Object.assign({}, comment, { votes: comment.votes - 1 }) : comment });
         	
 		default:
 			return state;
